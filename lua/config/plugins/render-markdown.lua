05c48441504fc0b3a6a950dc9905f9741d5ecab5
Caspar Van Wijk <caspar@wecodegreen.com>
2025-10-11 17:27:48 +0200

    Improve markdown heading rendering

--- lua/config/plugins/render-markdown.lua
+++ lua/config/plugins/render-markdown.lua
@@ -5,20 +5,39 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     config = function()
+        local heading_pad = { 40, 25, 4, 4, 4, 4 }
         require('render-markdown').setup({
-            render_modes = { 'n', 'no', 'v', 'V', 'i', 'c', 't' },
+            render_modes = { 'n', 'no', 'v', 'V', 'i', 'R', 'c', 't' },
             anti_conceal = {
                 enabled = false,
             },
             heading = {
                 sign = false,
+                width = 'block',
                 icons = '',
-                -- border = true,
-                -- border_virtual = true,
+                backgrounds = 'CursorLine',
+                foregrounds = 'CursorLine',
+                -- Use indentation to show heading level.
+                left_pad = { heading_pad[1] - 2, heading_pad[2] - 3, heading_pad[3] - 4, heading_pad[4] - 5, heading_pad[5] - 6, heading_pad[6] - 7 }, -- Use indentation to show heading level.
+                -- Make text appear centered within the block background.
+                right_pad = { heading_pad[1], heading_pad[2], heading_pad[3], heading_pad[4], heading_pad[5], heading_pad[6] },                        -- Make text appear centered within the block background.
+                -- OLD:
+                -- icons = false, -- Setting icons to false keeps the # symbols.
+                -- sign = true,
+                -- signs = { '1', '2', '3', '4' },
+                -- left_margin = { 0, 1, 2, 3, 4, 5 },
+                -- border = { true, false },
+                -- border_virtual = { true, false },
+                -- border_prefix = true,
             },
             link = {
-                -- Turn on / off inline link icon rendering.
+                -- Turn off inline link icon rendering.
                 enabled = false,
+            },
+            checkbox = {
+                checked = {
+                    scope_highlight = '@markup.strikethrough',
+                },
             },
             code = {
                 sign = false,
@@ -26,13 +45,6 @@ return {
                 language_name = false,
                 border = 'thick',
             },
-        })
-        vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-            callback = function(args)
-                if vim.bo[args.buf].filetype == 'markdown' then
-                    vim.cmd('RenderMarkdown enable')
-                end
-            end,
         })
     end
 }
